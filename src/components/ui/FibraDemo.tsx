661c3a3d321143182cabb38cda0239a68f904c0a
Alan Jair Almaguer <47544063+alanjair226@users.noreply.github.com>
2024-04-10 18:28:24 -0600

    inicio de proyecto

---
+++ src/components/ui/FibraDemo.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { GoogleGeminiEffect } from "../ui/google-gemini-effect";
+
+export function FibraDemo() {
+  return (
+    <div className="flex justify-center items-center h-full w-full">
+      <GoogleGeminiEffect />
+    </div>
+  );
+}
